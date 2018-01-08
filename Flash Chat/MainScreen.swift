79fe7e54a5085b002ec67163a6c1dd46464759b7
Arpit Patel <arpit@Arpits-MacBook-Pro.local>
2018-01-08 14:09:57 -0500

    Added timer clock and time duration field for database

--- Flash Chat/MainScreen.swift
+++ Flash Chat/MainScreen.swift
@@ -15,8 +15,13 @@ class MainScreen: UIViewController {
     
     var counter : String = ""
     var timer : Timer!
+    var clockTimer : Timer!
     var const = 0
     var arr = ""
+    var minutes : Int =  0
+    var seconds : Int = 0
+    var fractions : Int = 0
+    var clocktext : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +30,45 @@ class MainScreen: UIViewController {
     
     @objc func updateEntry() {
         //while (const == 0) {
-            arr.append(counter +  "a")
+            arr.append(counter +  "-")
             print(counter)
             print(arr)
        // }
     }
+    
+    @IBOutlet weak var clock: UILabel!
+    
+    @objc func updateClock () {
+        
+        fractions += 1
+        if fractions == 100 {
+            
+            seconds += 1
+            fractions = 0
+        }
+        
+        if seconds == 60 {
+            
+            minutes += 1
+            seconds = 0
+        }
+        
+        let fractionString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
+        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        
+        clocktext = "\(minutesString):\(secondsString):\(fractionString)"
+        clock.text = clocktext
+    }
+    
     
     @IBOutlet weak var startPressed: UIButton!
     @IBOutlet weak var stopPressed: UIButton!
     
     @IBAction func startPressed(_ sender: AnyObject) {
         
-
+        clockTimer = Timer.scheduledTimer(timeInterval: 0.01.seconds, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+        
         timer = Timer.scheduledTimer(timeInterval: 10.seconds, target: self, selector: #selector(updateEntry), userInfo: nil, repeats: true)
         
   
@@ -50,8 +82,9 @@ class MainScreen: UIViewController {
         const = 1
     
         timer.invalidate()
+        clockTimer.invalidate()
         
-        let dataDictionary : [String:Any] = ["Time": arr , "Field1": "13.7"]
+        let dataDictionary : [String:Any] = ["Time": clocktext , "Field1": arr]
         
         print(arr)
         dataDB.childByAutoId().setValue(dataDictionary) {
